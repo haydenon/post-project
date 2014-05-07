944b64575041bac19f763907691f778ec8e0d879
Nathan Muckley <nathan.muckley@gmail.com>
2014-05-07 17:55:05 +1200

    Added rails scaffold

---
+++ kps/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kps::Application.config.secret_key_base = '8a88157d98cb2fab306e94267036370f190c75084eeb6ae7967076b8f6377163fe79248be6ea6ad197396011e3e5af48e2210f80af65578601cc784f0f64b338'
