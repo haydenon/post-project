c3df0d31d0c5faf587e8d6de3f5a754effccdc92
Chad Sandeman <chadsandeman@gmail.com>
2014-05-08 14:13:19 +1200

    Just making sure everythings working, set up kps/index and the controller for it

--- kps/config/routes.rb
+++ kps/config/routes.rb
@@ -1,9 +1,11 @@
 Kps::Application.routes.draw do
+  get "kps/index"
+  get 'kps/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'kps#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
