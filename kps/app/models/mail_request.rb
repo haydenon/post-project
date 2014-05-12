f484c39af79db23cf97b4d79bbf3bfeb3b4bb425
Hayden <hayden.oneill@paradise.net.nz>
2014-05-12 20:30:41 +1200

    changed MailRequest and RouteSegment to contain methods to access the segments for a request and vice versa

--- kps/app/models/mail_request.rb
+++ kps/app/models/mail_request.rb
@@ -1,6 +1,6 @@
 class MailRequest < ActiveRecord::Base
 	belongs_to :priority, :class_name =>"Priority", :foreign_key => 'priority_id' 
 	belongs_to :mail_route, :class_name =>"MailRoute", :foreign_key => 'mail_route_id'
-	has_many :MailRequest_RouteSegments
-	has_many :RouteSegments, through: :MailRequest_RouteSegments
+	has_many :mail_request_route_segments, :class_name => "MailRequestRouteSegment"
+	has_many :segments, :class_name => "RouteSegment", :through => :mail_request_route_segments
 end
--- kps/app/models/route_segment.rb
+++ kps/app/models/route_segment.rb
@@ -2,8 +2,8 @@ class RouteSegment < ActiveRecord::Base
 	belongs_to :toLocation, :class_name =>"Location", :foreign_key => 'to_id'
 	belongs_to :fromLocation, :class_name =>"Location", :foreign_key => 'from_id'
 	belongs_to :company, :class_name =>"Company", :foreign_key => 'company_id'
-	has_many :MailRequest_RouteSegments
-	has_many :MailRequests, through: :MailRequest_RouteSegments
+	has_many :mail_request_route_segments, :class_name => "MailRequestRouteSegment"
+	has_many :requests, :class_name => "MailRequest", :through => :mail_request_route_segments
 	
 	
 	validate :check_locations
