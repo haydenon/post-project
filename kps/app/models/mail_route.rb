436cbfa9e77e6e0e0b0bb55dd62db93b5acee81b
Chad Sandeman <chadsandeman@gmail.com>
2014-05-12 17:20:16 +1200

    Updated MailRequest_RouteSegments view to display a bunch of information about both attributes

--- kps/app/models/mail_route.rb
+++ kps/app/models/mail_route.rb
@@ -3,5 +3,10 @@ class MailRoute < ActiveRecord::Base
 	belongs_to :fromLocation, :class_name =>"Location", :foreign_key => 'from_id'
 	belongs_to :priority, :class_name =>"Priority", :foreign_key => 'priority_id'
 	has_many :mailRequest, :class_name =>"MailRequest", :foreign_key => 'mail_route_id'
+	
+	validate :check_locations
+	def check_locations
+		errors.add(:to_id, "Location Can't be the same as the From location") if to_id == from_id
+	end
 
 end
