e461d55b4220a683bd80d35bb1609975c3ddd57f
Hayden <hayden.oneill@paradise.net.nz>
2014-05-14 14:45:16 +1200

    changed names to better fit naming conventions. documentation reflects this.

--- kps/app/models/mail_route.rb
+++ kps/app/models/mail_route.rb
@@ -1,8 +1,8 @@
 class MailRoute < ActiveRecord::Base
-	belongs_to :toLocation, :class_name =>"Location", :foreign_key => 'to_id'
-	belongs_to :fromLocation, :class_name =>"Location", :foreign_key => 'from_id'
+	belongs_to :to_location, :class_name =>"Location", :foreign_key => 'to_id'
+	belongs_to :from_location, :class_name =>"Location", :foreign_key => 'from_id'
 	belongs_to :priority, :class_name =>"Priority", :foreign_key => 'priority_id'
-	has_many :mailRequest, :class_name =>"MailRequest", :foreign_key => 'mail_route_id'
+	has_many :mail_request, :class_name =>"MailRequest", :foreign_key => 'mail_route_id'
 	
 	validate :check_locations
 	def check_locations
--- kps/app/models/route_segment.rb
+++ kps/app/models/route_segment.rb
@@ -1,6 +1,6 @@
 class RouteSegment < ActiveRecord::Base	
-	belongs_to :toLocation, :class_name =>"Location", :foreign_key => 'to_id'
-	belongs_to :fromLocation, :class_name =>"Location", :foreign_key => 'from_id'
+	belongs_to :to_location, :class_name =>"Location", :foreign_key => 'to_id'
+	belongs_to :from_location, :class_name =>"Location", :foreign_key => 'from_id'
 	belongs_to :company, :class_name =>"Company", :foreign_key => 'company_id'
 	has_many :mail_request_route_segments, :class_name => "MailRequestRouteSegment"
 	has_many :requests, :class_name => "MailRequest", :through => :mail_request_route_segments
