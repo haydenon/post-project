0485409ee45541eeba743c272cf0859e8e746486
Hayden <hayden.oneill@paradise.net.nz>
2014-05-14 15:06:13 +1200

    changed properties to match ruby naming convention. documentation updated

--- kps/app/models/mail_route.rb
+++ kps/app/models/mail_route.rb
@@ -2,7 +2,7 @@ class MailRoute < ActiveRecord::Base
 	belongs_to :to_location, :class_name =>"Location", :foreign_key => 'to_id'
 	belongs_to :from_location, :class_name =>"Location", :foreign_key => 'from_id'
 	belongs_to :priority, :class_name =>"Priority", :foreign_key => 'priority_id'
-	has_many :mail_request, :class_name =>"MailRequest", :foreign_key => 'mail_route_id'
+	has_many :mail_requests, :class_name =>"MailRequest", :foreign_key => 'mail_route_id'
 	
 	validate :check_locations
 	def check_locations
