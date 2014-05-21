0903e93ca050ae724343df8b76c426f3641c2f7f
Hayden <hayden.oneill@paradise.net.nz>
2014-05-21 16:06:05 +1200

    Revert "Added fields order and reversed to MRRS class"
    This reverts commit 4aabb14a654718d35c7f7f86dd5552aa012decee.

--- kps/db/migrate/20140521022403_add_reversed_to_mail_request_route_segment.rb
+++
@@ -1,5 +0,0 @@
-class AddReversedToMailRequestRouteSegment < ActiveRecord::Migration
-  def change
-	add_column :mail_request_route_segments, :reversed, :boolean
-  end
-end
--- kps/db/migrate/20140521023014_add_order_to_mail_request_route_segment.rb
+++
@@ -1,6 +0,0 @@
-class AddOrderToMailRequestRouteSegment < ActiveRecord::Migration
-  
-  def changed
-	add_column :mail_request_route_segments, :order, :integer
-  end
-end
