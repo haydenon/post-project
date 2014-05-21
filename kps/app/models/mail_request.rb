674aa6b2174a367b5f56f46488c54789c4505a7d
Hayden <hayden.oneill@paradise.net.nz>
2014-05-22 10:58:02 +1200

    changed it so deleting a request cascades, and destroys the corresponding MailRequestRouteSegments. also updated sytlesheet to stop show pages causing sidebar to drop

--- kps/app/models/mail_request.rb
+++ kps/app/models/mail_request.rb
@@ -1,7 +1,7 @@
 class MailRequest < ActiveRecord::Base
 	belongs_to :priority, :class_name =>"Priority", :foreign_key => 'priority_id' 
 	belongs_to :mail_route, :class_name =>"MailRoute", :foreign_key => 'mail_route_id'
-	has_many :mail_request_route_segments, :class_name => "MailRequestRouteSegment"
+	has_many :mail_request_route_segments, :class_name => "MailRequestRouteSegment", :dependent => :destroy
 	has_many :segments, :class_name => "RouteSegment", :through => :mail_request_route_segments
 
 	attr_accessor :to_id #FOR FORM USE ONLY
