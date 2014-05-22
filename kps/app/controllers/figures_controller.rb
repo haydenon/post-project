e678acca7690d1df3d4126423281c633a184f421
Hayden <hayden.oneill@paradise.net.nz>
2014-05-22 21:17:39 +1200

    added event number figures to page

--- kps/app/controllers/figures_controller.rb
+++ kps/app/controllers/figures_controller.rb
@@ -5,8 +5,10 @@ class FiguresController < ApplicationController
 
   # GET /figures
   def index
-    result = Figures.get_revenue_expenditure DateTime.now
+    time = DateTime.now
+    result = Figures.get_revenue_expenditure time
     @revenue = result[0]
     @expenditure = result[1]
+    @events = Figures.get_events_to_time time
   end
 end
--- kps/lib/helpers/figures.rb
+++ kps/lib/helpers/figures.rb
@@ -1,13 +1,19 @@
 class Figures
+
 	def self.get_revenue_expenditure (time)
 		revenue = 0
     	expenditure = 0
-    	mailrrs = MailRequestRouteSegment.where("created_at <= ?",
-  		time)
+    	mailrrs = MailRequestRouteSegment.where("created_at <= ?", time)
     	mailrrs.each do |mrrs|
       		revenue = revenue + mrrs.price
       		expenditure = expenditure + mrrs.cost
       	end
       	return [revenue, expenditure]
 	end
+
+	def self.get_events_to_time (time)
+    	events = Event.where("created_at <= ?", time)
+      	return events.size
+	end
+
 end
