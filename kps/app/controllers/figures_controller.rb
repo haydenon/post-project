3cd6c4aabd22a2c47aa4bcb0041aa0749bb587ce
Hayden <hayden.oneill@paradise.net.nz>
2014-05-22 20:46:14 +1200

    added rev and expend to figure page

--- kps/app/controllers/figures_controller.rb
+++ kps/app/controllers/figures_controller.rb
@@ -1,6 +1,15 @@
+require 'helpers/helper'
+
 class FiguresController < ApplicationController
 
   # GET /figures
   def index
+    @mrrs = MailRequestRouteSegment.all
+    @revenue = 0
+    @expenditure = 0
+    @mrrs.each do |mrrs|
+      @revenue = @revenue + mrrs.price
+      @expenditure = @expenditure + mrrs.cost
+    end
   end
 end
