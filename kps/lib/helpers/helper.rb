d4fd51c2d5182cc1b766a6001b2f58510ac79a0d
Hayden <hayden.oneill@paradise.net.nz>
2014-05-22 13:50:20 +1200

    changed view formatting

--- kps/lib/helpers/helper.rb
+++ kps/lib/helpers/helper.rb
@@ -1,9 +1,18 @@
 require 'action_view'
+require 'helpers/time_helper'
 include ActionView::Helpers::NumberHelper
 
 class Helper
 	include ActionView::Helpers::NumberHelper
 	def self.decimal_as_currency (dec=0.0)
 		return '$' + number_with_precision(dec, :precision => 2)
+	end
+
+	def self.time_format (time)
+		return time.strftime("%d/%m/%y %H:%M%Z")		
+	end
+
+	def self.time_day (time)
+		return time.strftime("%d/%m/%y")		
 	end
 end
