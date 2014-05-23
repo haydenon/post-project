38b680bd70bc13432ec176e8633d47e311a56ed6
Chad Sandeman <chadsandeman@gmail.com>
2014-05-23 22:50:15 +1200

    fixed validation checks to deal with nil inputs

--- kps/app/models/route_segment.rb
+++ kps/app/models/route_segment.rb
@@ -11,12 +11,12 @@ class RouteSegment < ActiveRecord::Base
 	def check_validation
 		errors.add(:to_id, "Location Can't be the same as the From location") if to_id == from_id
 		errors.add(:day, "Not a valid day integer must be 0 - 6") if day >= 7 or day < 0
-		errors.add(:frequency, "Frequency must be greater than 0") if frequency < 1	
-		errors.add(:duration, "Duration must be greater than 0") if duration < 1
+		errors.add(:frequency, "Frequency must be greater than 0") if frequency == nil or frequency < 1	
+		errors.add(:duration, "Duration must be greater than 0") if duration == nil or duration < 1
 		errors.add(:priority_id, "does not exist") if  (!Priority.exists?(priority_id))
 		# errors.add(:cost, "must be greater than 0") if cost < 1	
-		errors.add(:costVolume, "must be greater than 0") if costVolume <= 0	
-		errors.add(:costWeight, "must be greater than 0") if costWeight <= 0	
+		errors.add(:costVolume, "must be greater than 0") if costVolume == nil or costVolume <= 0	
+		errors.add(:costWeight, "must be greater than 0") if costWeight == nil or costWeight <= 0	
 		errors.add(:to_id, "does not exist") if (!Location.exists?(to_id))
 		errors.add(:from_id, "does not exist") if (!Location.exists?(from_id))
 		errors.add(:company_id, "does not exist") if (!Company.exists?(company_id))
