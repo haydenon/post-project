542d98d06a73b7ce09bd3210608c716e5767742d
Chad Sandeman <chadsandeman@gmail.com>
2014-05-23 21:45:18 +1200

    added another validation check to company creation

--- kps/app/models/company.rb
+++ kps/app/models/company.rb
@@ -2,5 +2,6 @@ class Company < ActiveRecord::Base
 	has_many :route_segments, :class_name =>"RouteSegment", :foreign_key => 'company_id'
 	
 	validates :name, uniqueness: { case_sensitive: false}
+	validates :name, :length => { :minimum => 2}
 	
 end
